c8287690b23b60d39662953343184921813ccdf4
RupamDas-lt <rupamd@lambdatest.com>
2023-09-24 23:40:00 +0530

    Mod-1

--- loginPageScript.js
+++ loginPageScript.js
@@ -1,20 +1,47 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const loginForm = document.getElementById('login-form');
+//     const usernameInput = document.getElementById('username');
+//     const passwordInput = document.getElementById('password');
+//     const loginStatus = document.getElementById('login-status');
+
+//     loginForm.addEventListener('submit', function (e) {
+//         e.preventDefault();
+//         const enteredUsername = usernameInput.value;
+//         const enteredPassword = passwordInput.value;
+
+//         if (enteredUsername === 'admin' && enteredPassword === 'admin') {
+//             loginStatus.textContent = 'Login Successful!';
+//             loginStatus.style.color = '#007BFF';
+//         } else {
+//             loginStatus.textContent = 'Incorrect username or password.';
+//             loginStatus.style.color = '#ff0000';
+//         }
+//     });
+// });
 document.addEventListener('DOMContentLoaded', function () {
     const loginForm = document.getElementById('login-form');
+    const loginContainer = document.getElementById('login-container');
+    const messageDiv = document.getElementById('message');
     const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
-    const loginStatus = document.getElementById('login-status');
 
     loginForm.addEventListener('submit', function (e) {
         e.preventDefault();
         const enteredUsername = usernameInput.value;
         const enteredPassword = passwordInput.value;
-
+        loginContainer.style.display = 'none';
         if (enteredUsername === 'admin' && enteredPassword === 'admin') {
-            loginStatus.textContent = 'Login Successful!';
-            loginStatus.style.color = '#007BFF';
+            // Display a success message
+            messageDiv.style.display = 'block';
+            messageDiv.textContent = 'Login Successful!';
+            messageDiv.style.color = '#fff';
+            messageDiv.style.background='#27AE60';
         } else {
-            loginStatus.textContent = 'Incorrect username or password.';
-            loginStatus.style.color = '#ff0000';
+            // Display an error message
+            messageDiv.style.display = 'block';
+            messageDiv.textContent = 'Incorrect username or password.';
+            messageDiv.style.color = '#fff';
+            messageDiv.style.background='#C0392B';
         }
     });
 });
